f48488d3ebc948f55284a56a4bba7db8afc0b092
J3SUNG <wptjd6141@naver.com>
2021-02-12 03:22:15 -0800

    useState

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import React from "react";
-import axios from "axios";
+import React, { useState } from "react";
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  };
-  getMovies = async () => {
-    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
-    console.log(movies);
-  };
-  componentDidMount() {
-    this.getMovies();
-  }
-  render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
-  }
-}
+const App = () => {
+  const [count, setCount] = useState(0);
+  const counter = () => setCount(count + 1);
+
+  return (
+    <div>
+      <div>{count}</div>
+      <button onClick={counter}> Click </button>
+    </div>
+  );
+};
+
 export default App;
