8e3f0359d1723938aebfb553ae21a4eaec2ed5d4
J3SUNG <wptjd6141@naver.com>
2021-02-13 03:27:28 -0800

    useRef

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  const counter = () => setCount(count + 1);
+  const [id, setId] = useState();
+  const [pw, setPw] = useState();
+  const idRef = useRef();
+  const pwRef = useRef();
+
+  const loginRequest = (event) => {
+    event.preventDefault();
+
+    console.log(idRef.current);
+    console.log(pwRef.current);
+  };
+  const onChange = (event) => {
+    console.log(event.target);
+    console.log(event.target.value);
+    setId = event.target.value;
+  };
+
+  console.log(idRef);
+  console.log(pwRef);
 
   return (
-    <div>
-      <div>{count}</div>
-      <button onClick={counter}> Click </button>
-    </div>
+    <form onSubmit={loginRequest}>
+      <label>
+        id:
+        <input id="id" ref={idRef} type="text" value={id} onChange={onChange} />
+      </label>
+      <label>
+        pw:
+        <input
+          pw="pw"
+          ref={pwRef}
+          type="password"
+          value={pw}
+          onChange={onChange}
+        />
+      </label>
+      <button type="submit">Login</button>
+    </form>
   );
 };
 
