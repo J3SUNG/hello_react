54e2a2b5f7a8e3247bfd366de78e326a86c27878
J3SUNG <wptjd6141@naver.com>
2021-02-17 05:34:20 -0800

    useContext

--- src/App.js
+++ src/App.js
@@ -1,30 +1,63 @@
-import React, { useMemo, useRef, useState, useCallback } from "react";
+import React, { createContext, useContext } from "react";
+
+// 0. AppContext 생성
+const AppContext = createContext();
 
 const App = () => {
-  const [input, setInput] = useState("");
-  const inputRef = useRef();
-  const printInput = () => {
-    console.log({ input });
-    return input;
+  const user = {
+    nickname: "danuel",
+    isAdmin: true,
   };
-  const onChange = (event) => {
-    const {
-      target: { value },
-    } = event;
 
-    setInput(value);
-  };
-  const value = useCallback(() => printInput(), []);
+  return (
+    <AppContext.Provider value={user}>
+      <div>
+        <Posts />
+      </div>
+    </AppContext.Provider>
+  );
+};
+
+// 1. PostsContext 생성
+const PostsContext = createContext();
+
+const Posts = () => {
+  const posts = [
+    {
+      title: "useContext 알아보기",
+      content: "이번 편에서는 React Context를 ...",
+    },
+  ];
+
+  return (
+    <PostsContext.Provider value={posts}>
+      <Children />
+    </PostsContext.Provider>
+  );
+};
+
+// 2. user와 posts를 가져와 화면에 보여주기
+const Children = () => {
+  const user = useContext(AppContext);
+  const posts = useContext(PostsContext);
+
+  let label = "user";
+  if (user.isAdmin) {
+    label = "admin";
+  }
+
   return (
     <div>
-      <input value={input} ref={inputRef} onChange={onChange} />
-      <button
-        onClick={() => {
-          value();
-        }}
-      >
-        Print
-      </button>
+      <div>{label}</div>
+      <div>{user.nickname}</div>
+      <div>
+        {posts.map((post, index) => (
+          <div key={index}>
+            <div>{post.title}</div>
+            <div>{post.content}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
