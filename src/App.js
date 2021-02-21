60e1719fddde2c6d2cc5ef1dd84140e40831c63a
J3SUNG <wptjd6141@naver.com>
2021-02-21 02:07:20 -0800

    addTodo

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React, { useState } from "react";
 import List from "./List.jsx";
 function App() {
   const [todos, setTodos] = useState(["study"]);
+  const [newTodo, setNewTodo] = useState();
+
+  const changeInputData = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos(...todos, newTodo);
+  };
+
   return (
     <>
       <h1>todo list</h1>
       <form action="">
-        {" "}
-        <input type="text" name="" />
-        <button>add todo</button>
+        <input type="text" name="" onChnage={changeInputData} />
+        <button onClick={addTodo}>add todo</button>
       </form>
       <List todos={todos} />
     </>
