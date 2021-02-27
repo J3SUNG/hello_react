10b67811d17beb0459c96d7806cd0d4e5905f7a7
J3SUNG <wptjd6141@naver.com>
2021-02-26 17:40:56 -0800

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { useState, useEffect, useCallback, useMemo } from "react";
 
 const App = () => {
+  const [count, setCount] = useState(30);
   const [flag, setFlag] = useState(false);
   const [input, setInput] = useState("");
   const printInput = useCallback(() => {
-    return Math.random();
-  }, [flag]);
+    return Math.random() + count;
+  }, []);
   const onChange = (event) => {
     setInput(event.target.value);
   };
   const onClick = (event) => {
     event.preventDefault();
     setFlag(!flag);
+    setCount(count + 1);
+    console.log(count);
   };
-  useEffect(() => console.log(printInput()));
+  useEffect(() => {
+    console.log(printInput());
+  });
 
   return (
     <>
