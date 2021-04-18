3ef4c4197abdac646d31248adafec93c05f3c0b8
J3SUNG <wptjd6141@naver.com>
2021-04-17 23:34:00 -0700

    Event

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -2,16 +2,23 @@ import React, { useState } from "react";
 
 const EventPractice = () => {
   const [message, setMessage] = useState("");
+  const onClick = () => {
+    alert(message);
+    setMessage("");
+  };
+  const onChange = (e) => {
+    setMessage(e.target.value);
+  };
   return (
     <div>
       <h1>Event Practice</h1>
+      <button onClick={onClick}>Button</button>
       <input
         type="text"
         name="message"
         placeholder="input anything"
-        onChange={(e) => {
-          setMessage(e.target.value);
-        }}
+        value={message}
+        onChange={onChange}
       />
       <div>{message}</div>
     </div>
