b388a7bb957f9e390f354aef391d8bc78c24b47f
J3SUNG <wptjd6141@naver.com>
2021-04-17 23:41:33 -0700

    add EventListener

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -2,24 +2,43 @@ import React, { useState } from "react";
 
 const EventPractice = () => {
   const [message, setMessage] = useState("");
+  const [username, setUsername] = useState("");
   const onClick = () => {
-    alert(message);
+    alert(username + ": " + message);
+    setUsername("");
     setMessage("");
   };
   const onChange = (e) => {
-    setMessage(e.target.value);
+    e.target.name == "username"
+      ? setUsername(e.target.value)
+      : setMessage(e.target.value);
+  };
+  const pressKey = (e) => {
+    if (e.key === "Enter") {
+      onChange();
+    }
   };
   return (
     <div>
       <h1>Event Practice</h1>
-      <button onClick={onClick}>Button</button>
+      <input
+        type="text"
+        name="username"
+        placeholder="username"
+        value={username}
+        onChange={onChange}
+        onKeyPress={pressKey}
+      />
       <input
         type="text"
         name="message"
         placeholder="input anything"
         value={message}
         onChange={onChange}
+        onKeyPress={pressKey}
       />
+      <button onClick={onClick}>Button</button>
+      <div>{username}</div>
       <div>{message}</div>
     </div>
   );
