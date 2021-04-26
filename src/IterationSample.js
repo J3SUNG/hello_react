818e88553111d99ebbd0593a6d5332a306797844
J3SUNG <wptjd6141@naver.com>
2021-04-25 22:40:04 -0700

    map

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const IterationSample = () => {
-  const names = ["snowman", "ice", "snow", "wind"];
-  const result = names.map((name, index) => <li key={index}>{name}</li>);
-  console.log(result);
-  return <ul>{result}</ul>;
+  const [names, setNames] = useState([
+    {
+      id: 1,
+      text: "snowman",
+    },
+    {
+      id: 2,
+      text: "ice",
+    },
+    {
+      id: 3,
+      text: "snow",
+    },
+    {
+      id: 4,
+      text: "wind",
+    },
+  ]);
+  const [inputText, setInputText] = useState("");
+  const [nextId, setNextId] = useState(5);
+  const namesList = names.map((name) => <li key={name.id}>{name.text}</li>);
+  return <ul>{namesList}</ul>;
 };
 
 export default IterationSample;
