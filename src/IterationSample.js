0901e118ad15a36a4fbd9c2aeb5dd025f1f8091a
J3SUNG <wptjd6141@naver.com>
2021-04-25 22:31:13 -0700

    map

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const IterationSample = () => {
   const names = ["snowman", "ice", "snow", "wind"];
-  const result = names.map((name) => <li>{name}</li>);
+  const result = names.map((name, index) => <li key={index}>{name}</li>);
   console.log(result);
   return <ul>{result}</ul>;
 };
