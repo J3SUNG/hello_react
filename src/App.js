6532069aa79754bcad38dacc99b016996216937a
J3SUNG <wptjd6141@naver.com>
2021-05-08 06:16:06 -0700

    add logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
+import logo from "./logo.png";
 import "./App.css";
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
