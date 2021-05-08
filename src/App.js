afac34399be23cb31f951dd1ec10718db54630a4
J3SUNG <wptjd6141@naver.com>
2021-05-08 06:37:33 -0700

    Commit before npm eject

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React, { useState } from "react";
 import logo from "./logo.png";
 import "./App.css";
+import SassComponent from "./SassComponent";
 const App = () => {
   return (
     <div className="App">
-      <header>
-        <img src={logo} className="logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <SassComponent />
     </div>
   );
 };
