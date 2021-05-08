ed0c443b062c11a8133bec87452d567cf9d412e0
J3SUNG <wptjd6141@naver.com>
2021-05-08 06:04:41 -0700

    move file

--- src/App.js
+++ src/App.js
@@ -1,33 +1,8 @@
 import React, { useState } from "react";
-import Average from "./Average";
-import Counter from "./Counter";
-import EventPractice from "./EventPractice";
-import Info from "./Info";
-import IterationSample from "./IterationSample";
-import MyComponent from "./MyComponent";
-import RefSample from "./RefSample";
-import Say from "./Say";
-import ScrollBox from "./ScrollBox";
-import ValidationSample from "./ValidationSample";
-
 const App = () => {
-  // const name = "React";
-  // const id = undefined;
-  const [visible, setVisible] = useState(false);
   return (
     <>
-      {/* <div>{name === "React" && "Hello, React!"}</div> */}
-      {/* <div>{id || "ID is undefined."}</div> */}
-      {/* <MyComponent>Hello</MyComponent> */}
-      {/* <Say /> */}
-      {/* <EventPractice /> */}
-      {/* <ValidationSample /> */}
-      {/* <RefSample /> */}
-      {/* <ScrollBox /> */}
-      {/* <IterationSample /> */}
-      {/* <Counter /> */}
-      <Info />
-      {/* <Average /> */}
+      <div>Hello</div>
     </>
   );
 };
