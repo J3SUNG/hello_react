e65f0bb4ddb9903d5906373c0cb466457fa7f3b6
J3SUNG <wptjd6141@naver.com>
2021-05-18 10:25:29 -0700

    Algorithm

--- src/practice/App.js
+++ src/practice/App.js
@@ -2,7 +2,11 @@ import React, { useState } from "react";
 import "./App.css";
 import StyledComopnent from "./StyledComopnent";
 const App = () => {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <StyledComopnent />
+    </div>
+  );
 };
 
 export default App;
