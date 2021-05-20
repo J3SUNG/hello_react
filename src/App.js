ca4f4b5f8d15cdd21d957bb98e4d8766d9185a18
J3SUNG <wptjd6141@naver.com>
2021-05-19 20:19:20 -0700

    Todo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from "./components/TodoInsert";
 import TodoList from "./components/TodoList";
 import "./App.css";
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Start React",
+      checked: true,
+    },
+    {
+      id: 2,
+      text: "Style Component",
+      checked: true,
+    },
+    {
+      id: 3,
+      text: "Make a Todo App",
+      checked: false,
+    },
+  ]);
+
   return (
     <TodoTemplate>
       <TodoInsert />
-      <TodoList />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 };
